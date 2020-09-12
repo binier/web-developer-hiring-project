15be8d825939f196324b87f589d37459132fe23e
Zura Benashvili <zura.benashvili@gmail.com>
2020-09-12 11:39:47 +0400

    fix(client): clean angular default app component

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'loss-control-plus-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('loss-control-plus-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('loss-control-plus-client app is running!');
-  });
 });
