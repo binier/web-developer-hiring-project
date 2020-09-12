8a7ee6537a9a60c21341877edac065a80f8fa859
Zura Benashvili <zura.benashvili@gmail.com>
2020-09-12 20:51:00 +0400

    feat(client): add modal component

--- client/src/app/routes/home/home.module.ts
+++ client/src/app/routes/home/home.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
+import { ModalComponent } from '@shared/modal/modal.component';
 import { PoliciesTableComponent } from '@shared/policies-table/policies-table.component';
 
 @NgModule({
   declarations: [
     PoliciesTableComponent,
+    ModalComponent,
+
     HomeComponent
   ],
   imports: [
---
+++ client/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+  @Input() isOpen = false;
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscHandler() {
+    this.close();
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  open() {
+    this.isOpen = true;
+  }
+
+  close() {
+    this.isOpen = false;
+  }
+}
