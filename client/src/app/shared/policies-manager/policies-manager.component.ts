d891849569fdc1fce9a42cc337ace5be99c8037d
Zura Benashvili <zura.benashvili@gmail.com>
2020-09-13 11:39:02 +0400

    feat(client/policy-manager): display payments on payment count click

--- client/src/app/shared/policies-manager/policies-manager.component.ts
+++ client/src/app/shared/policies-manager/policies-manager.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { Policy } from '@app/types';
+import { BehaviorSubject } from 'rxjs';
 import { pluck } from 'rxjs/operators';
+import { Payment } from '@app/types';
+import { ModalComponent } from '../modal/modal.component';
 import { PolicyService } from '@app/services';
 
 @Component({
@@ -11,11 +14,29 @@ import { PolicyService } from '@app/services';
 })
 export class PoliciesManagerComponent implements OnInit {
   private getPolicies$ = this.policySrv.getPolicies();
+  currentPolicy$ = new BehaviorSubject<Policy>(null);
 
   policies$ = this.getPolicies$.pipe(pluck('policies'));
+
+  @ViewChild('paymentsModel', { static: true })
+  paymentsModel: ModalComponent;
 
   constructor(private policySrv: PolicyService) { }
 
   ngOnInit(): void {
+  }
+
+  paymentsSum({ payments }: { payments: Payment[] }): number {
+    return payments.reduce((r, x) => r + x.payment_amount, 0);
+  }
+
+  policyClick({ policy, column }: {
+    policy: Policy,
+    column: string,
+  }): void {
+    if (column === 'payments') {
+      this.currentPolicy$.next(policy);
+      this.paymentsModel.open();
+    }
   }
 }
